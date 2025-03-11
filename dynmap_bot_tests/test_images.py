51998e4b55e7b45ee565498415196431c574ab96
Zackareee <github@zackaree.com>
2025-03-11 11:22:32 +1000

    Additional Tests

--- dynmap_bot_tests/test_images.py
+++ dynmap_bot_tests/test_images.py
@@ -38,3 +38,35 @@ class TestCropMapAndImage(TestBase):
         cropped_image: Image = image.crop_map_and_image(map_obj, image_obj)
 
         self.assert_images(cropped_image, expected_image)
+
+
+class TestBuildImageWithMap(TestBase):
+    def test_build_map_with_spawn_towns(self):
+        expected_image = Image.open(
+            f"{self.TESTDIR}/snapshots/test_build_map_with_spawn_towns.png"
+        )
+
+        map_obj: Map = misc.build_map(town_names=["Sanctuary", "Gulf_Of_Guinea"])
+        image_obj: Image = misc.build_image_with_map(map_obj)
+
+        self.assert_images(image_obj, expected_image)
+
+    def test_build_map_with_nation_france(self) -> None:
+        expected_image = Image.open(
+            f"{self.TESTDIR}/snapshots/test_build_map_with_nation_france.png"
+        )
+
+        map_obj: Map = misc.build_nation(nation_name="France")
+        image_obj: Image = misc.build_image_with_map(map_obj)
+
+        self.assert_images(image_obj, expected_image)
+
+    def test_build_map_with_town_on_region_border(self) -> None:
+        expected_image = Image.open(
+            f"{self.TESTDIR}/snapshots/test_build_map_with_town_on_region_border.png"
+        )
+
+        map_obj: Map = misc.build_map(town_names=["Limerick"])
+        image_obj: Image = misc.build_image_with_map(map_obj)
+
+        self.assert_images(image_obj, expected_image)
